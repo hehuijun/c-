b97e74d1186802f4d42a6ce88a5c6cefda211d90
hehuijun <he.huijun@gmail.com>
2016-11-06 10:21:49 +0800

    Create 0514.cpp

---
+++ 0514.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+class X
+{
+friend class Y;
+public:
+void Set(int i)
+{x=i;}
+void Display()
+{cout<<"x="<<x<<",";
+cout<<"y="<<y<<endl;
+}
+private:
+int x;
+static int y;
+};
+class Y
+
+{
+public:
+Y(int i,int j);
+void Display();
+private:
+X a;
+};
+int X::y=1;
+Y::Y(int i,int j)
+{
+a.x=i;
+X::y=j;
+}
+void Y::Display()
+{
+cout<<"x="<<a.x<<",";
+cout<<"y="<<X::y<<endl;
+}
+int main() {
+X b;
+b.Set(5);
+b.Display();
+Y c(6,9);
+c.Display();
+b.Display();
+}
