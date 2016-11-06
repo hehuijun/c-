cc76901b75088e8942811701b9c59b5d9bf9311e
hehuijun <he.huijun@gmail.com>
2016-11-06 11:25:55 +0800

    Update 0601.cpp

--- 0601.cpp
+++ 0601.cpp
@@ -13,13 +13,14 @@ private:
 int a;  
 };
 
-int main() {
-A x(8);
-int A::*pc;
-pc=&A::c;
+int main()
+{
+A x(8);//a=8
+int A::*pc;//指向一个类A的成员对象，没有确定指向哪个变量
+pc=&A::c;//定义一个指向类A的数据成员c的指针pc
 x.*pc=3;//等价于x.c=3;
-int(A:: * pfun)(int);
-pfun=A::fun;
-A * p=&x;
-cout<<(p->*pfun(5)<<endl;//等价于p->fun(5);
+int(A:: * pfun)(int);//定义一个指向类A的成员函数fun的指针pfun，等价于A::fun;
+pfun=A::fun;//error"invalid use of non-static member function ,无效使用非静态成员函数"
+A * p=&x;//a是类A的一个对象，p是指向对象x的指针,
+cout<<(p->* pfun)(5)<<endl;//等价于p->fun(5);
 }
