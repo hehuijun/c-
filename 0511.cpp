5797269d5614f221ee6d40105ed454815b8fad8a
hehuijun <he.huijun@gmail.com>
2016-11-06 09:38:43 +0800

    Create 0511.cpp

---
+++ 0511.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+class M
+{
+public:
+M(int a)
+{A=a;B=B+a;}
+static void f1(M m);
+private:
+ int A;
+ static int B;
+};
+void M::f1(M m)
+{
+  cout<<"A="<<m.A<<endl;//jingtaichengyuan
+  cout<<"B="<<B<<endl;
+  }
+ int M::B=0;
+ 
+    
+int main() {
+M P(5),Q(10);
+M::f1(P);
+M::f1(Q);
+}
